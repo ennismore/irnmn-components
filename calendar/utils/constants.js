f03bd60d7bc9ca76a05f304da14215f32d67b82a
Samuel Silva <hello@samuelsilva.pt>
2024-10-04 20:12:25 +0100

    Define constants in a separate file

---
+++ calendar/utils/constants.js
@@ -0,0 +1,12 @@
+export const CLASS_NAMES = {
+    panel: 'irnmn-calendar__panel',
+    inputGroup: 'irnmn-calendar__input-group',
+    month: 'irnmn-calendar__month',
+    monthTitle: 'irnmn-calendar__month__title',
+    daysContainer: 'irnmn-calendar__days',
+    dayBtn: 'irnmn-calendar__day-btn',
+    checkin: 'is-checkin',
+    checkout: 'is-checkout',
+    inRange: 'is-in-range',
+    resetBtn: 'irnmn-calendar__reset-btn'
+};
