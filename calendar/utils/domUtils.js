0576ef18c913b9fbf09942188cae53bec4c97ca8
Samuel Silva <hello@samuelsilva.pt>
2024-10-07 14:50:47 +0100

    Add role and aria-label

--- calendar/utils/domUtils.js
+++ calendar/utils/domUtils.js
@@ -3,15 +3,20 @@ import { CLASS_NAMES } from './constants.js';
 export function createMonthElement(month) {
     const monthEl = document.createElement('div');
     monthEl.classList.add(CLASS_NAMES.month);
+    monthEl.setAttribute('role', 'region');
+    monthEl.setAttribute('aria-labelledby', `month-${month.year}-${month.month}`); 
 
     const monthTitle = document.createElement('p');
     monthTitle.classList.add(CLASS_NAMES.monthTitle);
+    monthTitle.id = `month-${month.year}-${month.month}`;
     monthTitle.textContent = new Intl.DateTimeFormat('en-GB', { month: 'long' }).format(
         new Date(month.year, month.month)
     );
+    monthTitle.setAttribute('aria-label', `Month of ${monthTitle.textContent}`);
 
     const daysContainer = document.createElement('div');
     daysContainer.className = CLASS_NAMES.daysContainer;
+    daysContainer.setAttribute('role', 'grid');
 
     monthEl.appendChild(monthTitle);
     monthEl.appendChild(daysContainer);
@@ -25,9 +30,14 @@ export function createDayButton(day) {
     dayBtn.textContent = day.day;
     dayBtn.dataset.time = day.date.getTime();
     dayBtn.classList.add(CLASS_NAMES.dayBtn);
+    dayBtn.setAttribute('role', 'gridcell');
+
+    dayBtn.setAttribute('aria-label', `Select date ${new Intl.DateTimeFormat('en-GB', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' }).format(day.date)}`);
+
     return dayBtn;
 }
 
 export function toggleElementDisplay(element, isVisible) {
     element.style.display = isVisible ? 'block' : 'none';
+    element.setAttribute('aria-hidden', !isVisible); 
 }
