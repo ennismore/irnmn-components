1c8809dce02e98367fb901ec8060482a054a6652
Samuel Silva <hello@samuelsilva.pt>
2024-10-21 17:14:53 +0100

    attrs

---
+++ location/utils/constants.js
@@ -0,0 +1,10 @@
+const prefix = 'irnmn-location';
+
+export const CLASS_NAMES = {
+    locationDropdown: `${prefix}__dropdown`,
+    locationOption: `${prefix}__option`,
+    locationLabel: `${prefix}__label`,
+    locationContainer: `${prefix}__container`,
+    locationSelect: `${prefix}__select`,
+    locationErrorMessage: `${prefix}__error-message`
+};
