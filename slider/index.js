87a053b09ef1d9e29db2ed75edd88b40238c0f67
Samuel Silva <hello@samuelsilva.pt>
2024-11-25 10:02:31 +0000

    Handle single case and added accessibility attributes

--- slider/index.js
+++ slider/index.js
@@ -57,28 +57,77 @@ class IRNMNSlider extends HTMLElement {
             console.error(error);
             return;
         }
+        // Accessbility attributes
+        swipeContainer.setAttribute('role', 'region');
+        swipeContainer.setAttribute('aria-label', 'Slideshow with multiple slides');
+    
         const slides = swipeContainer.querySelectorAll(this.CLASSNAMES.SLIDES);
         const totalSlides = slides.length;
-
+    
+        /**
+         * Handle single slide case
+         */
+        if (totalSlides === 1) {
+            this.renderSingleSlide(slides[0]);
+            return;
+        }
+    
         if (this.dataset.sliderInitialized === "true") {
             return;
         }
         this.dataset.sliderInitialized = "true";
+    
+        this.cloneSlides(swipeContainer, slides);
+        this.updateTotalSlides(totalSlides);
+        this.initializePosition(swipeContainer);
+        this.addEventListeners(swipeContainer, totalSlides);
+    }
 
-        /**
-         * Handle slider loopping
-         */
+    /**
+     * Fall back to a single slide if only one slide is present
+     * 
+     * @param {HTMLElement} singleSlide - The single slide element
+     * @returns {void}
+     */
+    renderSingleSlide(singleSlide) {
+        singleSlide.style.cssText = 'display: block; margin: 0 auto;'; // Inline styles for single slide
+    
+        // Remove navigation and pagination controls if they exist
+        this.querySelector(this.CLASSNAMES.PREV_BUTTON)?.remove();
+        this.querySelector(this.CLASSNAMES.NEXT_BUTTON)?.remove();
+        this.querySelector(this.CLASSNAMES.CURRENT_SLIDE)?.remove();
+    }
+
+    /**
+     * Clone first and last slides to enable looping
+     */
+    cloneSlides(swipeContainer, slides) {
         const firstClone = slides[0].cloneNode(true);
         const lastClone = slides[slides.length - 1].cloneNode(true);
         swipeContainer.appendChild(firstClone); // Add the first clone at the end
         swipeContainer.insertBefore(lastClone, slides[0]); // Add the last clone at the beginning
+    }
 
-        // Update total slides including clones
-        const allSlides = swipeContainer.querySelectorAll(this.CLASSNAMES.SLIDES);
-        const clonedSlidesCount = allSlides.length;
-
+    /**
+     * Update the total number of slides
+     * 
+     * @param {number} totalSlides - The total number of slides
+     * @returns {void}
+     */
+    updateTotalSlides(totalSlides) {
         this.querySelector(this.CLASSNAMES.TOTAL_SLIDES).textContent = totalSlides;
+    }
 
+    /**
+     * Initialize the slider position
+     * 
+     * @param {HTMLElement} swipeContainer - The swipe container element
+     * @returns {void}
+     */
+    initializePosition(swipeContainer) {
+        this.currentSlide = 1;
+        swipeContainer.style.transform = `translateX(-${this.currentSlide * 100}%)`;
+    }
 
     /**
      * Add event listeners for navigation and touch/drag
@@ -121,46 +170,65 @@ class IRNMNSlider extends HTMLElement {
             this.eventListeners.push({ element, event, handler });
         }
     }
+
+    /**
+     * Update slides position and pagination
+     */
+    updateSlides(swipeContainer, clonedSlidesCount, totalSlides) {
+        swipeContainer.style.transition = 'transform 0.3s ease';
+        swipeContainer.style.transform = `translateX(-${this.currentSlide * 100}%)`;
+
+        const displayedSlideIndex =
+            this.currentSlide === 0
+                ? totalSlides
+                : this.currentSlide === clonedSlidesCount - 1
+                    ? 1
                     : this.currentSlide;
-            this.querySelector(this.CLASSNAMES.CURRENT_SLIDE).textContent = displayedSlideIndex;
-        };
+        this.querySelector(this.CLASSNAMES.CURRENT_SLIDE).textContent = displayedSlideIndex;
+    }
 
-        
-        /**
-         * Reset the position on the transition End
-         * listener: transitionend
-         */
-        const resetPosition = () => {
-            if (this.currentSlide === 0) {
-                this.currentSlide = totalSlides; // Jump to the last real slide
-                swipeContainer.style.transition = 'none'; // Disable animation
-                swipeContainer.style.transform = `translateX(-${this.currentSlide * 100}%)`;
-            } else if (this.currentSlide === clonedSlidesCount - 1) {
-                this.currentSlide = 1; // Jump to the first real slide
-                swipeContainer.style.transition = 'none'; // Disable animation
-                swipeContainer.style.transform = `translateX(-${this.currentSlide * 100}%)`;
-            }
-        };
+    /**
+     * Reset the position on the transition end
+     */
+    resetPosition(swipeContainer, clonedSlidesCount, totalSlides) {
+        if (this.currentSlide === 0) {
+            this.currentSlide = totalSlides;
+            swipeContainer.style.transition = 'none';
+            swipeContainer.style.transform = `translateX(-${this.currentSlide * 100}%)`;
+        } else if (this.currentSlide === clonedSlidesCount - 1) {
+            this.currentSlide = 1;
+            swipeContainer.style.transition = 'none';
+            swipeContainer.style.transform = `translateX(-${this.currentSlide * 100}%)`;
+        }
+    }
 
-        /**
-         * Move to the next slide
-         * listener: click
-         */
-        const nextSlide = () => {
-            this.currentSlide = (this.currentSlide + 1) % clonedSlidesCount;
-            updateSlides();
-        };
+    /**
+     * Move to the next slide
+     */
+    moveToNextSlide(updateSlides, clonedSlidesCount) {
+        this.currentSlide = (this.currentSlide + 1) % clonedSlidesCount;
+        updateSlides();
+    }
 
-        /**
-         * Move to the previous slide
-         * listener: click
-         */
-        const prevSlide = () => {
-            this.currentSlide = (this.currentSlide - 1 + clonedSlidesCount) % clonedSlidesCount;
-            updateSlides();
-        };
+    /**
+     * Move to the previous slide
+     */
+    moveToPrevSlide(updateSlides, clonedSlidesCount) {
+        this.currentSlide = (this.currentSlide - 1 + clonedSlidesCount) % clonedSlidesCount;
+        updateSlides();
+    }
 
-      
+    /**
+     * Setup drag and drop (or touch) event listeners
+     * 
+     * @param {HTMLElement} swipeContainer - The swipe container element
+     * @param {Function} nextSlide - The function to move to the next slide
+     * @param {Function} prevSlide - The function to move to the previous slide
+     * @param {Function} updateSlides - The function to update the slides
+     * 
+     * @returns {void}
+     */
+    setupDragAndDrop(swipeContainer, nextSlide, prevSlide, updateSlides) {
         let startX = 0;
         let currentX = 0;
         let isDragging = false;
@@ -168,7 +236,7 @@ class IRNMNSlider extends HTMLElement {
         const touchStart = (e) => {
             startX = e.touches ? e.touches[0].clientX : e.clientX;
             isDragging = true;
-            swipeContainer.style.transition = 'none'; // Disable transition during swipe
+            swipeContainer.style.transition = 'none';
         };
 
         const touchMove = (e) => {
@@ -179,10 +247,10 @@ class IRNMNSlider extends HTMLElement {
 
         const touchEnd = () => {
             if (!isDragging) return;
-            swipeContainer.style.transition = 'transform 0.3s ease'; // Re-enable transition
+            swipeContainer.style.transition = 'transform 0.3s ease';
             if (currentX > 50) prevSlide();
             else if (currentX < -50) nextSlide();
-            else updateSlides(); // Reset to the current slide if swipe distance is insufficient
+            else updateSlides();
             currentX = 0;
             isDragging = false;
         };
