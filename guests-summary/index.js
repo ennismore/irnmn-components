e0d4e8f87ed89d896389de9157785f359e4273e6
Elliot C <ejcromwell@gmail.com>
2025-01-16 15:45:20 +0000

    UPDATE: Add hidden input fields to the guest summary to that when a form is submitted from the booking bar, the room values are still passed to URL. Needed to adjust the html output of the summary text in order to append the html rather than overwrite it.

--- guests-summary/index.js
+++ guests-summary/index.js
@@ -95,6 +95,7 @@ class IRNMNGuestsSummary extends HTMLElement {
     updateSummary(event) {
         handleSyncEvent(event, this.summary, (newState) => {
             this.updateSummaryData(newState);
+            this.updateInputValues(newState);
             this.render();
         });
     }
@@ -158,7 +159,71 @@ class IRNMNGuestsSummary extends HTMLElement {
             );
         }
 
-        this.innerHTML = `<p>${summaryText}</p>`;
+        let summarySpan = this.querySelector('.guest-summary');
+
+        if (summarySpan) {
+           summarySpan.textContent = summaryText;
+           return;
+        }
+
+        summarySpan = document.createElement('span');
+        summarySpan.className = 'guest-summary';
+        summarySpan.textContent = summaryText;
+        this.insertAdjacentHTML('beforeend', summarySpan.outerHTML);
+    }
+
+    /**
+     * Create hidden input field.
+     *
+     * @param {HTMLElement} form
+     * @param {string} name
+     * @param {string} value
+     *
+     * @returns {HTMLElement} The hidden input element
+     */
+    createInput(name, value) {
+        let input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = name;
+        input.value = value;
+        return input;
+    }
+
+
+    /**
+     * Updates the DOM with hidden input elements based on the provided state.
+     *
+     * This method first removes all existing hidden input elements. It then iterates
+     * over the `newState` array to create and append hidden inputs for each room's
+     * adult count, child count, and the ages of each child.
+     *
+     * @param {Array<Object>} rooms - The new state representing room configurations.
+     * @param {number} rooms[].adults - The number of adults in the room.
+     * @param {number} rooms[].children - The number of children in the room.
+     * @param {Array<number>} rooms[].childrenAges - An array containing the ages of the children in the room.
+     *
+     */
+    updateInputValues(newState) {
+         this.querySelectorAll('input[type="hidden"]').forEach(input => input.remove());
+
+        newState.forEach((room, index) => {
+            const adultsInput = this.createInput(`rooms[${index}].adults`, room.adults);
+            this.insertAdjacentHTML('beforeend', adultsInput.outerHTML);
+
+            if (room.children > 0) {
+                const childrenInput = this.createInput(`rooms[${index}].children`, room.children);
+                this.insertAdjacentHTML('beforeend', childrenInput.outerHTML);
+
+                // This is needed since the storage value does not remove the
+                // childrenAges array when children are removed from the room.
+                // TODO: Update the storage value to remove the childrenAges array when children are removed.
+                for (let ageIndex = 0; ageIndex < room.children; ageIndex++) {
+                    const age = room.childrenAges[ageIndex];
+                    const childAgeInput = this.createInput(`rooms[${index}].childrenAges[${ageIndex}]`, age);
+                    this.insertAdjacentHTML('beforeend', childAgeInput.outerHTML);
+                };
+            }
+        });
     }
 
     appendSummary(summaryText, count, singularLabel, pluralLabel) {
