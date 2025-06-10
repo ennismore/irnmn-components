2c6f376540d9428597671236638b216913585983
Dávid Borsodi <corten.mgs@gmail.com>
2025-06-10 15:37:42 +0200

    feat/storybook-integration: added rest of the stories

--- stories/Page.stories.js
+++ stories/Page.stories.js
@@ -2,19 +2,19 @@ import * as HeaderStories from './Header.stories';
 import { Page } from './Page';
 
 export default {
-  title: 'Example/Page',
-  render: (args) => Page(args),
+    title: 'Example/Page',
+    render: (args) => Page(args),
 };
 
 export const LoggedIn = {
-  args: {
-    // More on composing args: https://storybook.js.org/docs/writing-stories/args#args-composition
-    ...HeaderStories.LoggedIn.args,
-  },
+    args: {
+        // More on composing args: https://storybook.js.org/docs/writing-stories/args#args-composition
+        ...HeaderStories.LoggedIn.args,
+    },
 };
 
 export const LoggedOut = {
-  args: {
-    ...HeaderStories.LoggedOut.args,
-  },
+    args: {
+        ...HeaderStories.LoggedOut.args,
+    },
 };
