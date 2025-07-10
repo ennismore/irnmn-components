2ac0a35ea157d9259a392f7546d525a0ccc8c54a
Christos <info@chrismartatos.com>
2025-07-10 15:13:29 +0300

    UPDATE: console log

--- location/index.js
+++ location/index.js
@@ -356,10 +356,16 @@ class IRNMNLocation extends HTMLElement {
                     try {
                         const url = new URL(attributeValue);
 
+                        console.log('url:', url);
+                        console.log('url.href:', url.href);
+
                         // Replace {langID} in the URL with the current HTML lang attribute
                         this.replaceLangIDInUrl(url);
+
                         console.log('4:', url.href);
+
                         this.parentForm.action = url.href;
+
                         console.log('5:', this.parentForm.action);
                     } catch (e) {
                         console.error('Invalid URL for externalServiceUrl', e);
@@ -401,7 +407,8 @@ class IRNMNLocation extends HTMLElement {
         )[0];
 
         console.log('langAttr:', langAttr);
-        console.log('Original URL:', url.href);
+        console.log('Original URL:', url);
+        console.log('Original url.href:', url.href);
 
         // Replace all encoded and unencoded {langID} (case-insensitive)
         // Handles both %7B/%7D (uppercase and lowercase) and curly braces
@@ -410,7 +417,8 @@ class IRNMNLocation extends HTMLElement {
             langAttr,
         );
 
-        console.log('Updated URL:', url.href);
+        console.log('Updated URL:', url);
+        console.log('Updated URL.href:', url.href);
         return url.href;
     }
 
